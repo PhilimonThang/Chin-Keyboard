f4322a1e52e7f26cb616a93a033e1511feff6b45
PhilT <thang.zoramthanga@gmail.com>
2023-09-07 19:22:09 -0400

    Initial Commit

---
+++ Chin-Keyboard/Chin_KeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chin_KeyboardApp.swift
+//  Chin-Keyboard
+//
+//  Created by macos on 9/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Chin_KeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
